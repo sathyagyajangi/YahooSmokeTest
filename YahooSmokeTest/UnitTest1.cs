11583c4be66d7d5edc129051cf64e86e242351d0
Sathyanarayana Gyajangi <Sathyanarayana@sportzinteractive.com>
2019-12-14 18:57:58 +0530

    first commit

--- YahooSmokeTest/UnitTest1.cs
+++ YahooSmokeTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -18,17 +19,19 @@ namespace YahooSmokeTest
 
             driver.Manage().Window.Maximize();
 
-            string handle = driver.CurrentWindowHandle;
+           
 
             for (int i = 2; i <= 11; i++)
             {
 
+                string title = driver.FindElement(By.XPath("/html/body/div[1]/header/div/section/div/div[3]/div/nav/ul/li[" + i + "]/a")).GetAttribute("title");
 
-                string title = driver.Title;
+                FunctionLibrary.waitForElement(driver, "/html/body/div[1]/header/div/section/div/div[3]/div/nav/ul/li[" + i + "]/a");
+
 
                 FunctionLibrary.clickAction(driver, "/html/body/div[1]/header/div/section/div/div[3]/div/nav/ul/li[" + i + "]/a", "xpath");
 
-                
+
 
                 Console.WriteLine(title);
 
@@ -40,16 +43,17 @@ namespace YahooSmokeTest
                 {
 
 
+                    driver.Navigate().GoToUrl("https://beta-cricket-yahoo.sportz.io/series/sri-lanka-tour-of-australia-2019-1111");
 
+                    Thread.Sleep(2000);
 
-                    driver.FindElement(By.XPath("//*[@id='ff810ec5-1cca-4f8d-8d74-f5676bf6f436']/div/div[2]/div[2]/a[2]")).Click();
-
-                    for (int j = 2; j <= 9; j++)
+                    for (int j = 2; j <= 10; j++)
                     {
 
+                        FunctionLibrary.waitForElement(driver, "/html/body/div[1]/div/myapp/section[1]/div/div/div/div/section/div/div/div[2]/div/a[" + j + "]");
 
-                        FunctionLibrary.clickAction(driver,"/html/body/div[1]/div/myapp/section[1]/div/div/div/div/section/div/div/div[2]/div/a[" + j + "]", "xpath");
-
+                        FunctionLibrary.clickAction(driver, "/html/body/div[1]/div/myapp/section[1]/div/div/div/div/section/div/div/div[2]/div/a[" + j + "]", "xpath");
+                                                                    
                        
                         Console.WriteLine(driver.Title);
 
@@ -64,7 +68,7 @@ namespace YahooSmokeTest
                     driver.Navigate().Back();
                 }
 
-                driver.Close();
+               
 
             }
 
@@ -72,7 +76,7 @@ namespace YahooSmokeTest
 
 
 
-
+            driver.Close();
 
 
         }
