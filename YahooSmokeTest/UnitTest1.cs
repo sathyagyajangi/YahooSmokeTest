391035aee47bca70f505579d9420babe3908b168
Sathyanarayana Gyajangi <Sathyanarayana@sportzinteractive.com>
2020-01-21 12:25:26 +0530

    first H

--- YahooSmokeTest/UnitTest1.cs
+++ YahooSmokeTest/UnitTest1.cs
@@ -16,13 +16,13 @@ namespace YahooSmokeTest
 
 
 
-        ExtentHtmlReporter reporter = new ExtentHtmlReporter("C:\\Users\\Satyanarayan\\source\\git\\YahooSmokeTest\\YahooSmokeTest\\Reports\\Local\\" + FunctionLibrary.Genaratedate() + "\\yahoo.html");
+        ExtentHtmlReporter reporter = new ExtentHtmlReporter("C:\\Users\\Satyanarayan\\source\\git\\YahooSmokeTest\\YahooSmokeTest\\Reports\\prod\\" + FunctionLibrary.Genaratedate() + "\\yahoo.html");
         ExtentReports extent = new ExtentReports();
 
 
         [TestMethod]
       
-        public void Local() 
+        public void prod() 
         {
 
             IWebDriver driver = new ChromeDriver();
@@ -35,16 +35,16 @@ namespace YahooSmokeTest
             {
               
 
-                driver.Url = "http://192.168.100.119:9097/";
+                driver.Url = "https://cricket.yahoo.net";
 
                 driver.Manage().Window.Maximize();
 
 
 
-                for (int i = 2; i <= 10; i++)
+                for (int i = 2; i <= 11; i++)
                 {
 
-                    string title = driver.FindElement(By.XPath("/html/body/div[1]/header/div/section/div/div[3]/div/nav/ul/li[" + i + "]/a")).GetAttribute("title");
+                    string title = driver.FindElement(By.XPath("//*[@class='site-nav']/ul/li[" + i + "]/a")).GetAttribute("title");
 
 
                     string actTitle = FunctionLibrary.ReadDataExcel(1, 1, i);
@@ -53,10 +53,10 @@ namespace YahooSmokeTest
 
                     test.Log(Status.Info, "Title verified");
 
-                    FunctionLibrary.waitForElement(driver, "/html/body/div[1]/header/div/section/div/div[3]/div/nav/ul/li[" + i + "]/a");
+                    FunctionLibrary.waitForElement(driver, "//*[@class='site-nav']/ul/li[" + i + "]");
 
 
-                    FunctionLibrary.clickAction(driver, "/html/body/div[1]/header/div/section/div/div[3]/div/nav/ul/li[" + i + "]/a", "xpath");
+                    FunctionLibrary.clickAction(driver, "//*[@class='site-nav']/ul/li[" + i + "]", "xpath");
 
 
 
@@ -72,15 +72,15 @@ namespace YahooSmokeTest
                     {
 
 
-                        driver.Navigate().GoToUrl("http://192.168.100.119:9097//series/big-bash-league-2019-20-1182");
+                        driver.Navigate().GoToUrl("https://cricket.yahoo.net/series/big-bash-league-2019-20-1182");
 
                         Thread.Sleep(2000);
 
                         for (int j = 2; j <= 10; j++)
                         {
-                            FunctionLibrary.waitForElement(driver, "/html/body/div[1]/div/myapp/section[1]/div/div/div/div/section/div/div/div[2]/div/a[" + j + "]");
+                            FunctionLibrary.waitForElement(driver, "//*[@class='quick-links-menu swiper-container']/div/a[" + j + "]");
 
-                            FunctionLibrary.clickAction(driver, "/html/body/div[1]/div/myapp/section[1]/div/div/div/div/section/div/div/div[2]/div/a[" + j + "]", "xpath");
+                            FunctionLibrary.clickAction(driver, "//*[@class='quick-links-menu swiper-container']/div/a[" + j + "]", "xpath");
 
                             test.Log(Status.Pass, driver.Title +  "Test case Passsed");
 
@@ -127,91 +127,6 @@ namespace YahooSmokeTest
 
         }
 
-        [TestMethod]
-        public void beta()
-        {
-
-            IWebDriver driver = new ChromeDriver();
-
-
-           
-            extent.AttachReporter(reporter);
-
-            var test = extent.CreateTest("yahoobeta");
-
-            try
-            {
-       
-
-                driver.Url = "http://beta.cricket.yahoo.sportz.io/";
-
-                driver.Manage().Window.Maximize();
-
-
-
-                for (int i = 2; i <= 10; i++)
-                {
-
-                    string title = driver.FindElement(By.XPath("//nav[@class='site-nav']/ul/li[" + i + "]/a")).GetAttribute("title");
-
-                    FunctionLibrary.waitForElement(driver, "//nav[@class='site-nav']/ul/li[" + i + "]/a");
-
-
-                    FunctionLibrary.clickAction(driver, "//nav[@class='site-nav']/ul/li[" + i + "]/a", "xpath");
-
-
-
-                    Console.WriteLine(title);
-
-                    test.Log(Status.Pass, title + "Test case Passsed");
-
-
-
-                    if (title.Contains("Series"))
-                    {
-
-
-                        driver.Navigate().GoToUrl("https://beta.cricket.yahoo.sportz.io/series/big-bash-league-2019-20-1182");
-
-                        Thread.Sleep(2000);
-                                                                
-                        for (int j = 2; j <= 10; j++)
-                        {
-                            FunctionLibrary.waitForElement(driver, "//div[@class='swiper-wrapper']/a[" + j + "]");
-
-                            FunctionLibrary.clickAction(driver, "//div[@class='swiper-wrapper']/a[" + j + "]", "xpath");
-
-                            test.Log(Status.Pass, driver.Title + "Test case Passsed");
-                            Console.WriteLine(driver.Title);
-
-                        }
-
-
-                    }
-
-
-
-
-                }
-                driver.Close();
-            }
-
-
-
-            catch (Exception)
-            {
-                Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
-
-                ss.SaveAsFile(@"C:\Users\Satyanarayan\source\git\YahooSmokeTest\YahooSmokeTest\Screenshot\Beta\" +FunctionLibrary.Genaratedate() + ".png");
-                test.Log(Status.Fail,driver.Title +"Test case failed");
-            }
-
-
-
-
-
-            extent.Flush();
-        }
 
       
     }
